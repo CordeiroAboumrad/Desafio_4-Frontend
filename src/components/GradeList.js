15a5add87f70afef8ad3d418558dab49ae0c74c7
jean.aboumrad <cordeiro.aboumrad@gmail.com>
2020-07-06 00:08:41 -0300

    Mudanças para id

--- src/components/GradeList.js
+++ src/components/GradeList.js
@@ -155,12 +155,12 @@ const GradeList = () => {
               {currentGrade.value}
             </div>
 
-            <Link
+            <Grade
               to={'/grade/' + currentGrade._id}
               className="badge badge-warning"
-            >
+            />
               Edit
-            </Link>
+            {/* </Link> */}
           </div>
         ) : (
           <div>
